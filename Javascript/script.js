7fed3f86f566ed3f08543e87ef860d269daada81
Ryan Large <101679941+RyanLarge13@users.noreply.github.com>
2022-07-05 03:33:48 -0700

    Updating paragraph for mobile views in resume section
    I needed a new function for handling paragraphs when in mobile view. Took away display none and am beginning to integrate the paragraphs for each icon on mobile

--- Javascript/script.js
+++ Javascript/script.js
@@ -180,33 +180,10 @@ const paraSlider = (count, e) => {
         skillParagraphs[count + 4].style.opacity = '0';
         skillParagraphs[count + 4].style.transform = 'translateX(10em)';
     }
-};
+}
 
 rightArrow.addEventListener('click', slider);
 leftArrow.addEventListener('click', slider);
-
-const paraSliderMobile = () => {
-    if (start > move) {
-        if (count === -3) {
-            skillParagraphs[skillParagraphs.length - 1].style.opacity = '0';
-            skillParagraphs[skillParagraphs.length - 1].style.transform = 'translateX(10em)';
-        }
-        skillParagraphs[count + 3].style.opacity = '1';
-        skillParagraphs[count + 3].style.transform = 'translateX(0) translateY(-50%)';
-        skillParagraphs[count + 2].style.opacity = '0';
-        skillParagraphs[count + 2].style.transform = 'translateX(10em)';
-    }
-    if (start < move) {
-        if (count === 3) {
-            skillParagraphs[0].style.opacity = '0';
-            skillParagraphs[0].style.transform = 'translateX(10em)';
-        }
-        skillParagraphs[count + 3].style.opacity = '1';
-        skillParagraphs[count + 3].style.transform = 'translateX(0) translateY(-50%)';
-        skillParagraphs[count + 4].style.opacity = '0';
-        skillParagraphs[count + 4].style.transform = 'translateX(10em)';
-    }
-};
 
 const sliderMobile = () => {
     if (start > move) {
@@ -244,8 +221,7 @@ const sliderMobile = () => {
             }
         });
     }
-    paraSliderMobile();
-};
+}
 
 skillParent.addEventListener('touchstart', (e) => {
     start = e.touches[0].clientX;
