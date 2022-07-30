475dee5ecf9563375e9366c7d6f4bc684be82a74
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-07-30 13:34:00 -0700

    Creating an index.js file for node.js and fetching data
    I also created functionality for my about section to display information based on sidebar icons

--- Javascript/aboutme.js
+++ Javascript/aboutme.js
@@ -1,3 +1,37 @@
 const sideBarIcons = document.querySelectorAll('.side-bar i');
 const infoSections = document.querySelectorAll('.info-grid-container div');
 
+sideBarIcons[0].style.color = '#ffe259';
+infoSections[0].style.display = 'block';
+infoSections[0].style.opacity = '1';
+
+const color = () => {
+    sideBarIcons.forEach((icon) => {
+        icon.style.color = '#000';
+    });
+};
+
+const pullUpInfo = (className) => {
+    for (let k = 0; k < infoSections.length; k++) {
+        if (infoSections[k].id === className) {
+            infoSections[k].style.display = 'block';             
+            setTimeout(() => {
+                infoSections[k].style.opacity = '1';
+            }, 250);
+        } else {
+            infoSections[k].style.opacity = '0';
+            setTimeout(() => {
+                infoSections[k].style.display = 'none';
+            }, 250);
+        }
+    }
+};
+
+sideBarIcons.forEach((icon) => {
+    icon.addEventListener('click', () => {
+        color();
+        icon.style.color = '#ffe259';
+        const className = icon.className;
+        pullUpInfo(className);
+    });
+});
