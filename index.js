d909ebaf798b5015addb23cf0384748f72ac897b
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-07-31 14:23:28 -0700

    Testing

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express'), morgan = require('morgan'), path = require('path'), bodyParser = require('body-parser'), uuid = require('uuid');
-const app = express();
+// const express = require('express'), morgan = require('morgan'), path = require('path'), bodyParser = require('body-parser'), uuid = require('uuid');
+// const app = express();
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(morgan('common'));
+// app.use(bodyParser.json());
+// app.use(express.static('public'));
+// app.use(morgan('common'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'), (err) => {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', index.html)
-        }
-    });
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/index.html'), (err) => {
+//         if (err) {
+//             next(err);
+//         } else {
+//             console.log('Sent:', index.html)
+//         }
+//     });
+// });
 
-app.listen(8080, () => {
-    console.log('Your app is running on port 8080');
-});
+// app.listen(8080, () => {
+//     console.log('Your app is running on port 8080');
+// });
