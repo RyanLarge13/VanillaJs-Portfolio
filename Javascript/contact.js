77ff76844975ea5d8844f4adf7aa238147bb3437
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-08-07 01:43:05 -0700

    Adding in static data for work exprience, this data will be changed when the database is created and fully functional.

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -6,6 +6,7 @@ const tel = document.getElementById('tel');
 const email = document.getElementById('email');
 const text = document.getElementById('text');
 const notValid = document.querySelector('.not-valid');
+const contactSection = document.querySelector('.sec-5');
 
 tel.addEventListener('keyup', () => {
     const value = tel.value;
@@ -32,8 +33,38 @@ text.addEventListener('keyup', () => {
     length < 400 ? ((notValid.innerHTML = 'Not Valid'), (notValid.style.opacity = '0')) : '';
 });
 
+const showForm = () => {
+    const submissionDiv = document.createElement('div');
+    const closeBtn = document.createElement('button');
+    const confirmContainer = document.createElement('div');
+    const heading = document.createElement('h3');
+    const paragraph = document.createElement('p');
+    submissionDiv.className = 'submission-conformation';
+    closeBtn.className = 'close-me';
+    heading.innerHTML = 'Thank You!!';
+    paragraph.innerHTML = 'Your message has been received and I will be getting in touch with you as soon as possible!';
+    closeBtn.innerHTML = 'Close';
+    submissionDiv.appendChild(confirmContainer);
+    confirmContainer.appendChild(heading);
+    confirmContainer.appendChild(paragraph);
+    contactSection.appendChild(submissionDiv);
+    submissionDiv.appendChild(closeBtn);
+    
+    closeBtn.addEventListener('click', () => {
+        submissionDiv.style.display = 'none';
+    });
+};
+
 submitBtn.addEventListener('click', (e) => {
+    const conformation = document.querySelector('.submission-conformation');
+    if (conformation !== null && email.value === '' || tel.value === '' || text.value === '') {
+        notValid.innerHTML = 'Please fill out the form before submission';
+        notValid.style.opacity = '1';
+    }
     e.preventDefault();
-    form.submit();
+    if (notValid.style.opacity === '1') {
+        return alert('Your form is not valid. Please enter a valid email address & phone number');
+    }
     form.reset();
+    showForm();
 });
